ca8282459e4a1c4b3797ead2be76cfab06d71763
Aditya Soni <1984rinasoni@gmail.com>
2026-03-24 01:05:06 +0530

    Dropdown-added

--- FrontPage/script.js
+++ FrontPage/script.js
@@ -2,13 +2,18 @@ document.addEventListener("DOMContentLoaded",()=>{
 window.updateOptions = updateOptions;
 window.addInvestment = addInvestment;
 const text=document.getElementsByClassName("invs");
+const dropdown=document.querySelector("#dropdown")
 const addbtn=document.querySelector(".addinvs button");
 const listinvs=document.querySelector(".container");
 const blur = document.querySelector(".blur");
 const cross =document.querySelector(".cross");
+const invsCard = document.querySelector(".invs-card");
+  const storer=document.querySelector(".invs-card ol");
+let selectedInvestment = null;
 
 
 
+console.log(storer);
 addbtn.addEventListener("click",()=>{
 listinvs.classList.remove("hidden");
 blur.classList.remove("hidden");
@@ -20,14 +25,9 @@ cross.addEventListener("click",()=>{
 });
 let investors=JSON.parse(localStorage.getItem("investors"))||[];
 
-investors.forEach(investor =>render(investor));
+  
 
-
-
-function render(task){
-    const li=document.createElement("li");
-}
-
+// document.getElementById("addBtn").addEventListener("click", addInvestment);
 
 
 const stocks = ["Reliance", "TCS", "Infosys"];
@@ -42,7 +42,7 @@ const stocks = ["Reliance", "TCS", "Infosys"];
     investmentSelect.innerHTML = "<option>Select option</option>";
 
     let options = [];
-
+console.log("changed"); 
     if (type === "Stock") options = stocks;
     if (type === "Mutual Fund") options = mutualFunds;
     if (type === "Crypto") options = crypto;
@@ -56,24 +56,105 @@ const stocks = ["Reliance", "TCS", "Infosys"];
     });
   }
 
+
   function addInvestment() {
     const data = {
+      id:Date.now(),
       type: document.getElementById("type").value,
       name: document.getElementById("investment").value,
       amount: document.getElementById("amount").value,
       date: document.getElementById("date").value
     };
 
-    console.log(data);
+
+
+    if(data.type === "" ||
+  data.name === "" ||
+  data.name === "Select option" ||
+  data.amount.trim() === "" ||
+  data.date.trim() === ""){
+      alert("Please Enter All The Field");
+      return;
+    }
+    if (isNaN(data.amount) || data.amount <= 0) {
+  alert("Enter valid amount");
+  return;
+}
+
+else{
+  investors.push(data);
+  savetolocal();
+  renderTask(data);
+  document.getElementById("type").value="";
+  document.getElementById("investment").value="Select option";
+  document.getElementById("amount").value="";
+  document.getElementById("date").value="";
+  console.log(data);
+  listinvs.classList.add("hidden");
+    blur.classList.add("hidden");
     alert("Investment Added!");
+
+}
   }
 
 
+  function savetolocal() {
+    localStorage.setItem("investors", JSON.stringify(investors));
+  }
+
+
+const invsText = document.querySelector(".invs");
+
+  function renderTask(data){
+  const li = document.createElement("li");
+
+  li.innerHTML = `${data.name} - ₹${data.amount} 
+  <button class="dlt-btn">Remove</button>`;
+
+  const btn = li.querySelector(".dlt-btn");
+  li.addEventListener("click", () => {
+    //  store full object
+    selectedInvestment = data;
+
+    //  update UI
+    invsText.textContent = `${data.name}`;
+
+    console.log("Selected:", selectedInvestment); // for testing
+  });
+  btn.addEventListener("click", (e) => {
+    e.stopPropagation(); 
+    investors = investors.filter((t) => t.id !== data.id);
+    li.remove();
+    savetolocal();
+  });
+
+  storer.appendChild(li);
+}
+
+
+dropdown.addEventListener("click",()=>{
+  invsCard.classList.toggle("hidden");
+});
 
 
 
 
+// document.addEventListener("click", (e) => {
+//   if (!e.target.closest(".main-section")) {
+//     invsCard.classList.add("hidden");
+//     dropdown.classList.remove("rotate");
+//   }
+// });
+investors.forEach(investor => renderTask(investor));
 
 
 
+const swaps=document.querySelectorAll(".invs-card ol li");
+
+// swaps.forEach((swap)=>{
+// swaps.addEventListener("click",()=>{
+// swap.classList.add("invs");
+// swap.classList.remove("invs-card")
+// });
+// });
 });
